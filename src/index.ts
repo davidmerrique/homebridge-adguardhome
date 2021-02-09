08f93c5f2c66fc9219b875c7f770b8ee0ae075fb
David Merrique <david.merrique@gmail.com>
2021-02-09 10:54:19 -0500

    Update deps

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import got, { Got } from "got";
 
 let hap: HAP;
 
-export = (api: API) => {
+export = (api: API): void => {
   hap = api.hap;
   api.registerAccessory("homebridge-adguardhome", "AdGuardHome", AdGuardHome);
 };
@@ -36,7 +36,7 @@ class AdGuardHome implements AccessoryPlugin {
   private readonly switchService: Service;
   private readonly informationService: Service;
 
-  constructor(log: Logging, config: AccessoryConfig, api: API) {
+  constructor(log: Logging, config: AccessoryConfig) {
     this.log = log;
     this.name = config.name;
 
@@ -54,7 +54,7 @@ class AdGuardHome implements AccessoryPlugin {
     ).toString("base64")}`;
 
     this.gotInstance = got.extend({
-      prefixUrl: "http://" + this.host + ":" + this.port + "/control",
+      prefixUrl: `http://${this.host}:${this.port}/control`,
       responseType: "json",
       headers: {
         Authorization,
@@ -69,7 +69,7 @@ class AdGuardHome implements AccessoryPlugin {
         (callback: CharacteristicGetCallback) => {
           this.gotInstance("status")
             .json()
-            .then((body: any) => {
+            .then((body: unknown) => {
               const enabled = body.protection_enabled === true;
               this.log.info(
                 `Current state of the switch was returned: ${
@@ -79,7 +79,7 @@ class AdGuardHome implements AccessoryPlugin {
               callback(undefined, enabled);
             })
             .catch((error) => {
-              this.log.error(error.response.body);
+              if (error.response) this.log.error(error.response.body);
               callback(error);
             });
         }
@@ -101,7 +101,7 @@ class AdGuardHome implements AccessoryPlugin {
               callback(null, enabled);
             })
             .catch((error) => {
-              this.log.error(error.response.body);
+              if (error.response) this.log.error(error.response.body);
               callback(error);
             });
         }
@@ -119,80 +119,3 @@ class AdGuardHome implements AccessoryPlugin {
     return [this.informationService, this.switchService];
   }
 }
-
-// var Service, Characteristic;
-
-// module.exports = function (homebridge) {
-//   Service = homebridge.hap.Service;
-//   Characteristic = homebridge.hap.Characteristic;
-
-//   homebridge.registerAccessory("homebridge-adguard", "AdGuardHome", adguard);
-// };
-
-// function adguard(log, config) {
-//   this.log = log;
-//   global.log = log;
-
-//   this.manufacturer = "AdGuard";
-//   this.name = config["name"] || "AdGuardHome";
-
-//   this.username = config["username"];
-//   this.password = config["password"];
-//   this.host = config["host"] || "localhost";
-//   this.port = config["port"] || 80;
-
-//   const Authorization = `Basic ${Buffer.from(
-//     `${this.username}:${this.password}`
-//   ).toString("base64")}`;
-
-//   this.gotInstance = got.extend({
-//     prefixUrl: "http://" + this.host + ":" + this.port + "/control",
-//     headers: {
-//       Authorization,
-//     },
-//     responseType: "json",
-//   });
-// }
-
-// adguard.prototype.getServices = function () {
-//   var infoService = new Service.AccessoryInformation()
-//     .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-//     .setCharacteristic(Characteristic.Model, this.model)
-//     .setCharacteristic(Characteristic.SerialNumber, this.serial);
-
-//   var switchService = new Service.Switch(this.name);
-//   switchService
-//     .getCharacteristic(Characteristic.On)
-//     .on("get", this.getStatus.bind(this))
-//     .on("set", this.setStatus.bind(this));
-
-//   this.informationService = infoService;
-//   this.switchService = switchService;
-
-//   return [this.informationService, this.switchService];
-// };
-
-// adguard.prototype.getStatus = function (callback) {
-//   this.gotInstance("status")
-//     .then(({ body }) => {
-//       callback(null, body.protection_enabled === true);
-//     })
-//     .catch((error) => {
-//       callback(error);
-//     });
-// };
-
-// adguard.prototype.setStatus = function (newVal, callback) {
-//   this.gotInstance
-//     .post("dns_config", {
-//       json: {
-//         protection_enabled: !!newVal,
-//       },
-//     })
-//     .then((res) => {
-//       callback(null, res.status === 200);
-//     })
-//     .catch((error) => {
-//       callback(error);
-//     });
-// };
